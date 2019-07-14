34899883cb92ee23d8186d6fe199d7555173bd68
Loviisa Hurme <Loviisa@LAPTOP-SHTJBD7O.localdomain>
2019-07-14 15:38:06 +0300

    completed 1.1 and 1.2.

--- Osa01/kurssitiedot/src/index.js
+++ Osa01/kurssitiedot/src/index.js
@@ -1,5 +1,53 @@
+/*
+Mooc Fullstack 2019
+Loviisa Hurme
+Tehtävä 1.1 ja 1.2
+*/
+
+
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.course}</h1>
+		</div>
+		)
+}
+
+const Part = (props) => {
+
+	console.log(props.part)
+	return(
+		<div>
+
+			<p>
+			{props.part} {props.exercises}
+			</p>
+		</div>
+		)
+}
+
+
+const Content = (props) => {
+	return(
+		<div>
+		<Part part={props.part1} exercises={props.exercises1} />
+		<Part part={props.part2} exercises={props.exercises2} />
+		<Part part={props.part3} exercises={props.exercises3} />
+		</div>
+		)
+}
+
+const Total = (props) => {
+	return(
+		<div>
+			<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+		</div>
+		)
+}
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -12,17 +60,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content
+      part1={part1} exercises1={exercises1}
+      part2={part2} exercises2={exercises2}
+      part3={part3} exercises3={exercises3}
+      />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+
     </div>
   )
 }
