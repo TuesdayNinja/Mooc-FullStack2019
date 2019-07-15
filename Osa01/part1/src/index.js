84bf7e9844530d2d2861e7f443a9779acca885f7
Loviisa Hurme <Loviisa@LAPTOP-SHTJBD7O.localdomain>
2019-07-15 22:02:10 +0300

    modified part1 example app

--- Osa01/part1/src/index.js
+++ Osa01/part1/src/index.js
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
+  const setToValue = (value) => setCounter(value)
+
+
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <Display counter={counter}/>
+      <Button
+        handleClick={() => setToValue(counter + 1)}
+        text='plus'
+      />
+      <Button
+        handleClick={() => setToValue(counter - 1)}
+        text='minus'
+      />
+      <Button
+        handleClick={() => setToValue(0)}
+        text='zero'
+      />
     </div>
   )
 }
 
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </div>
-  )
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
