6922d1442b33fc2b88be3952d4dd9dc4edbeb34d
Loviisa Hurme <Loviisa@LAPTOP-SHTJBD7O.localdomain>
2019-07-29 19:54:52 +0300

    Created Unicafe app

--- Osa01/part1/src/index.js
+++ Osa01/part1/src/index.js
@@ -1,40 +1,62 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = ({ handleClick, text }) => (
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
 
-
-
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0)
-  const setToValue = (value) => setCounter(value)
-
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        handleClick={() => setToValue(counter + 1)}
-        text='plus'
-      />
-      <Button
-        handleClick={() => setToValue(counter - 1)}
-        text='minus'
-      />
-      <Button
-        handleClick={() => setToValue(0)}
-        text='zero'
-      />
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+                <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
   )
-}
 
+
+}
 
 ReactDOM.render(
   <App />, 
