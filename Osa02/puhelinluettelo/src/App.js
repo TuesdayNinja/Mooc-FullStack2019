70d1dea8bdd5d9b7cf1253b4aa5952ffa8ff26b3
Loviisa Hurme <Loviisa@roam-fi-251-204.uta.fi>
2019-09-17 14:28:50 +0300

    Finished 2.6

--- Osa02/puhelinluettelo/src/App.js
+++ Osa02/puhelinluettelo/src/App.js
@@ -6,23 +6,50 @@ const App = () => {
   ])
   const [ newName, setNewName ] = useState('')
 
+  const Person = ({person}) => {
+    return(
+      <div>
+        <p>{person.name}</p>
+      </div>
+    )
+  }
+
+  const Rows = ({persons}) => {
+    return(
+    persons.map(person => <Person key= {person.name} person={person}/>)
+    )
+  }
+
   const addName = (event) => {
      event.preventDefault()
+
+     const nameObject={
+      name: newName
+     }
+
+     setPersons(persons.concat(nameObject))
+     setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        <Rows persons={persons}/>
+      </ul>
     </div>
   )
 
